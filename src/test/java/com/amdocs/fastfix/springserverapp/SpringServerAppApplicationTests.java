07423c0b58753d7459697069d80f108263c51285
Sibendra30 <46244484+Sibendra30@users.noreply.github.com>
2019-05-13 19:03:15 +0530

    Add files via upload

---
+++ src/test/java/com/amdocs/fastfix/springserverapp/SpringServerAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.amdocs.fastfix.springserverapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringServerAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
